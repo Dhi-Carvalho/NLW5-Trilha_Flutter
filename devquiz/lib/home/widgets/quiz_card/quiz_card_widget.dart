9eafc59a4644cf605452a852c9f20439fd3ba336
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-04-26 16:29:28 -0300

    Creating and using state controls and management

--- devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,19 +5,24 @@ import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
-  const QuizCardWidget({Key? key}) : super(key: key);
+  final String title;
+  final String completed;
+  final double percent;
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.completed,
+    required this.percent,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 16, bottom: 22, left: 16, right: 16),
       decoration: BoxDecoration(
-        border: Border.fromBorderSide(
-          BorderSide(color: AppColors.border)
-        ),
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10)
-      ),
+          border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -29,8 +34,9 @@ class QuizCardWidget extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          Text("Gerenciamento de Estado",
-          style: AppTextStyles.heading15,
+          Text(
+            title,
+            style: AppTextStyles.heading15,
           ),
           SizedBox(
             height: 20,
@@ -39,16 +45,11 @@ class QuizCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: Text(
-                  "3 de 10",
-                  style: AppTextStyles.body11
-                ),
+                child: Text(completed, style: AppTextStyles.body11),
               ),
-              Expanded(
-                flex: 2,
-                child: ProgressIndicatorWidget(value: 0.3)
-            )
-          ],)
+              Expanded(flex: 2, child: ProgressIndicatorWidget(value: percent))
+            ],
+          )
         ],
       ),
     );
