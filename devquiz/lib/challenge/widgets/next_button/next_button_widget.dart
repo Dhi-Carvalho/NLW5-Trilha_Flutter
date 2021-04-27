81fc9234185de7128590cb39210f401a6c8291d4
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-04-27 20:19:57 -0300

    Result button creation

--- devquiz/lib/challenge/widgets/next_button/next_button_widget.dart
+++ devquiz/lib/challenge/widgets/next_button/next_button_widget.dart
@@ -24,10 +24,24 @@ class NextButtonWidget extends StatelessWidget {
         this.onTap = onTap,
         this.label = label;
 
+  NextButtonWidget.purple({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.purple,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.green,
+        this.onTap = onTap,
+        this.label = label;
+
   NextButtonWidget.white({required String label, required VoidCallback onTap})
       : this.backgroundColor = AppColors.white,
         this.fontColor = AppColors.grey,
         this.borderColor = AppColors.border,
+        this.onTap = onTap,
+        this.label = label;
+  
+  NextButtonWidget.noBorder({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.white,
+        this.fontColor = AppColors.grey,
+        this.borderColor = Colors.transparent,
         this.onTap = onTap,
         this.label = label;
 
